eb079c57f456d345dbcfe4035ba885dbed89bcc2
Alessio Roberto <darthpelo@gmail.com>
2016-10-23 20:20:19 +0200

    Remove model reference from VC

---
+++ i100/Protocols.swift
@@ -0,0 +1,24 @@
+//
+//  Protocols.swift
+//  i100
+//
+//  Created by Alessio Roberto on 23/10/16.
+//  Copyright © 2016 Alessio Roberto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Sharable {
+    func share(number: String) -> UIViewController
+}
+
+protocol Notification {
+    func addObserver(name aName: NotificationName)
+    
+    func catchNotification(notification:NSNotification)
+}
+
+protocol ViewProtocol: class {
+    func updateChess(at indexPath: IndexPath, with newValue: Int)
+}
